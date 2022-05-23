8641ba52db9130a9706854e4ea6de053bd3e3f94
orhunpusat <orhunpusat3000@gmail.com>
2022-05-24 00:38:08 +0300

    day6 alert

---
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T1_AlertPractices.java
@@ -0,0 +1,31 @@
+package com.cydeo.tests.day6_alerts_iframes_windows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T1_AlertPractices {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        //1. Open browser
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //2. Go to website: http://practice.cydeo.com/javascript_alerts
+        driver.get("http://practice.cydeo.com/javascript_alerts");
+    }
+
+    @Test
+    public void alert_test1(){
+        //3. Click to “Click for JS Alert” button
+        //4. Click to OK button from the alert
+        //5. Verify “You successfully clicked an alert” text is displayed
+    }
+}
