5abfbfbb19eea0956a72f33d123492f6b23870d3
orhunpusat <orhunpusat3000@gmail.com>
2022-05-24 15:04:39 +0300

    day6 Task4 iframe Task complete

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T4_IframePractice.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T4_IframePractice.java
@@ -32,7 +32,13 @@ public class T4_IframePractice {
         //4. Assert: “Your content goes here.” Text is displayed.
         //we need to switch drivers focus iframe
         //option #1- switching to iframe using id attribute value
-        driver.switchTo().frame("mce_0_ifr");
+        //driver.switchTo().frame("mce_0_ifr");
+
+        //option #2- passing index number of iframe
+        //driver.switchTo().frame(0);
+
+        //option #3-locate as web element and ğadd in frame() method
+        driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@id='mce_0_ifr']")));
 
         //locate the p tag
         WebElement yourContentGoesHereText = driver.findElement(By.xpath("//p"));
@@ -40,6 +46,14 @@ public class T4_IframePractice {
         Assert.assertTrue(yourContentGoesHereText.isDisplayed());
 
         //5. Assert: “An iFrame containing the TinyMCE WYSIWYG Editor”
+        //to be able to verify the header we must switch back to main "HTML"
+        driver.switchTo().parentFrame();
+
+        WebElement headerText = driver.findElement(By.xpath("//h3"));
+
+        Assert.assertTrue(headerText.isDisplayed());
+
+
     }
 
 
