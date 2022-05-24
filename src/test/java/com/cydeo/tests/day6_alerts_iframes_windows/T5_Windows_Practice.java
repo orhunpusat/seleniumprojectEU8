b01aa36525cddf3732b37e0cd3d3b77e6ac4c0da
orhunpusat <orhunpusat3000@gmail.com>
2022-05-24 16:22:46 +0300

    day6 Task5 Windows Task complete

---
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T5_Windows_Practice.java
@@ -0,0 +1,74 @@
+package com.cydeo.tests.day6_alerts_iframes_windows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class T5_Windows_Practice {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        //1-Open browser
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //2-Go to : https://practice.cydeo.com/windows
+        driver.get("https://practice.cydeo.com/windows");
+    }
+
+    @Test
+    public void multiple_window_test(){
+
+        //Storing the main pages window handle as string is good practice for future re-usable purposes
+        String mainHandle = driver.getWindowHandle();
+
+        System.out.println("mainHandle = " + mainHandle);
+
+        //4. Assert: Title is “Windows”
+        String expectedTitle = "Windows";
+        String actualTitle = driver.getTitle();
+
+        Assert.assertEquals(expectedTitle, actualTitle);
+
+        System.out.println("Title before click = " + actualTitle);
+
+        //5. Click to: “Click Here” link
+        WebElement clickHereLink = driver.findElement(By.linkText("Click Here"));
+        clickHereLink.click();
+
+        //6. Switch to new Window.
+        Set<String> allWindowHandles = driver.getWindowHandles();
+        //window handle 1- main window
+        //window handle 2- 2nd window
+
+        for (String each :allWindowHandles ) {
+
+            driver.switchTo().window(each);
+            System.out.println("Current title while switching windows: " +driver.getTitle());
+        }
+
+
+        //7. Assert: Title is “New Window”
+        String expextedTitleAfterClick = "New Window";
+        actualTitle = driver.getTitle();
+
+        Assert.assertEquals(actualTitle, expextedTitleAfterClick);
+
+        System.out.println("Title after click= " + actualTitle);
+    }
+    @AfterMethod
+    public void tearDown(){
+        driver.close();
+    }
+}
