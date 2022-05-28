0011ae7762ecbd5c7a5a7ec6ace19822ca3ae197
orhunpusat <orhunpusat3000@gmail.com>
2022-05-28 21:07:42 +0300

    day8 T1 web table practice method#1 complete

--- src/test/java/com/cydeo/tests/day8_properties_config_reader/T1_WebTable_Order_Verify.java
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/T1_WebTable_Order_Verify.java
@@ -1,6 +1,7 @@
 package com.cydeo.tests.day8_properties_config_reader;
 
 import com.cydeo.utilities.WebDriverFactory;
+import com.cydeo.utilities.WebTableUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -52,4 +53,22 @@ public class T1_WebTable_Order_Verify {
        Assert.assertEquals(actualDBobDate, expectedBobDate);
 
     }
+
+    //We use the utility meathod created
+    @Test
+    public void test2(){
+      String costumerOrderDate1 =  WebTableUtils.returnOrderDate8(driver,"Alexandra Gray");
+        System.out.println("costumerOrderDate1 = " + costumerOrderDate1);
+
+      String costumerOrderDate2 = WebTableUtils.returnOrderDate8(driver,"John Doe");
+        System.out.println("costumerOrderDate2 = " + costumerOrderDate2);
+
+    }
+
+    //Using WebTableUtils.orderVerify(); method
+    @Test
+    public void test3(){
+        WebTableUtils.orderVerify(driver,"Bart Fisher","01/16/2021");
+    }
+
 }
--- src/test/java/com/cydeo/utilities/WebTableUtils.java
+++ src/test/java/com/cydeo/utilities/WebTableUtils.java
@@ -3,6 +3,7 @@ package com.cydeo.utilities;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 public class WebTableUtils {
 
@@ -11,7 +12,7 @@ public class WebTableUtils {
     //• Return type: String
     //• Arg1: WebDriver driver
     //• Arg2: String costumerName
-    //This method should accept a costumerName and return the costumer order date
+    //This method should accept a customerName and return the costumer order date
     //as a String
 
     public static String returnOrderDate8(WebDriver driver, String customerName){
@@ -19,6 +20,24 @@ public class WebTableUtils {
         String locator = "//td[.='"+customerName+"']/following-sibling::td[3]";
         WebElement customerDateCell = driver.findElement(By.xpath(locator));
         return customerDateCell.getText();
+    }
+
+    //Method #2 info:
+    //• Name: orderVerify ()
+    //• Return type: void
+    //• Arg1: WebDriver driver
+    //• Arg2: String customerName
+    //• Arg3: String expectedOrderDate
+    //This method should accept above mentioned arguments and internally assert
+    //expectedOrderDate matching actualOrderDate
+
+    public static void orderVerify(WebDriver driver, String customerName, String expectedOrderDate){
+        String locator = "//td[.='"+customerName+"']/following-sibling::td[3]";
+        WebElement customerDateCell = driver.findElement(By.xpath(locator));
+
+        String actualOrderDate = customerDateCell.getText();
+
+        Assert.assertEquals(actualOrderDate,expectedOrderDate);
 
     }
 }
