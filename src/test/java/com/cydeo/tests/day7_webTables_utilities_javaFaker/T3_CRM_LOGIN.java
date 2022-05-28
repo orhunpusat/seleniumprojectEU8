4a76df5631b3e1b6a07420bc4457880095a6810e
orhunpusat <orhunpusat3000@gmail.com>
2022-05-28 12:24:18 +0300

    day7 T3 Before and Test method complete

--- src/test/java/com/cydeo/tests/day7_webTables_utilities_javaFaker/T3_CRM_LOGIN.java
+++ src/test/java/com/cydeo/tests/day7_webTables_utilities_javaFaker/T3_CRM_LOGIN.java
@@ -1,7 +1,10 @@
 package com.cydeo.tests.day7_webTables_utilities_javaFaker;
 
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -15,16 +18,33 @@ public class T3_CRM_LOGIN {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
-
+    //TC #3: Login scenario
     @Test
-    public void crm_login_test(){
-        //TC #3: Login scenario
+    public void crm_login_test() throws InterruptedException {
+
         //1. Create new test and make set ups
-        //2. Go to : http://login1.nextbasecrm.com/
+        //2. Go to : https://login1.nextbasecrm.com/
+        driver.get("https://login1.nextbasecrm.com/");
+
         //3. Enter valid username
+        Thread.sleep(2000);
+        WebElement inputUsername = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        inputUsername.sendKeys("Helpdesk1@cybertekschool.com");
+
+        //Helpdesk1@cybertekschool.com UserUser
+        //Helpdesk2@cybertekschool.com UserUser
+
         //4. Enter valid password
+        Thread.sleep(2000);
+        WebElement inputPassword = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        inputPassword.sendKeys("UserUser");
+
         //5. Click to `Log In` button
+        WebElement loginButton = driver.findElement(By.xpath("//input[@value='Log In']"));
+        loginButton.click();
+
         //6. Verify title is as expected:
         //Expected: Portal
+        BrowserUtils.verifyTitle(driver,"Portal");
     }
 }
