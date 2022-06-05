30e2a2598d8fe2b7f37ab322f44651abb7cc5cb6
orhunpusat <orhunpusat3000@gmail.com>
2022-06-05 21:58:57 +0300

    day9 singleton

---
+++ src/test/java/com/cydeo/tests/javafaker_testbase_driverUtil/SingletonPractice.java
@@ -0,0 +1,22 @@
+package com.cydeo.tests.javafaker_testbase_driverUtil;
+
+import org.testng.annotations.Test;
+
+public class SingletonPractice {
+
+    @Test
+    public void singleton_understand_test1(){
+
+        String str1 = Singleton.getWord();
+        System.out.println("str1 = " + str1);
+
+        String str2 = Singleton.getWord();
+        System.out.println("str2 = " + str2);
+
+        String str3 = Singleton.getWord();
+        System.out.println("str3 = " + str3);
+
+    }
+
+
+}
