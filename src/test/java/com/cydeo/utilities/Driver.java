98bfef99a7696ae2070231a8ac0ef824c2dfab89
orhunpusat <orhunpusat3000@gmail.com>
2022-06-05 09:29:17 +0300

    day9 driver.java

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -9,10 +9,22 @@ public class Driver {
      */
     private Driver(){}
 
+
     /*
     We make WebDriver private, because we want to close access from outside the class.
     We make it static because we will use it in a static method
      */
     private static WebDriver driver;
+
+
+    /*
+    Create a reusable utility method which will return same driver instance when we call it
+     */
+    public static WebDriver getDriver(){
+        if (driver==null){
+
+        }
+        return driver;
+    }
 
 }
