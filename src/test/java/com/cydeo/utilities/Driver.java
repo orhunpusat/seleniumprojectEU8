5a956fac2d17a6a0de2b52fc2c5421033b533bdc
orhunpusat <orhunpusat3000@gmail.com>
2022-06-05 09:19:28 +0300

    day9 driver

---
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -0,0 +1,12 @@
+package com.cydeo.utilities;
+
+public class Driver {
+
+    /*
+     Creating a private constructor, so we are closing access to the object of this class from outside of the class
+     */
+    private Driver(){
+
+    }
+
+}
