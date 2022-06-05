e945046b1be79b4741955166888ac894bf35898e
orhunpusat <orhunpusat3000@gmail.com>
2022-06-05 09:45:22 +0300

    day9 driver.java

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -1,6 +1,11 @@
 package com.cydeo.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
@@ -22,6 +27,22 @@ public class Driver {
      */
     public static WebDriver getDriver(){
         if (driver==null){
+            String browserType = ConfigurationReader.getProperty("browser");//We read our browserType from configuration.properties.
+                            //This way we can control which browser is opened from outside our code from configuration.properties.
+            switch (browserType){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+            }
 
         }
         return driver;
