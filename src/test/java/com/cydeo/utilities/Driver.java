dbc55f4e9374e836ddb9facd4a86ebf872dea864
orhunpusat <orhunpusat3000@gmail.com>
2022-06-05 09:24:29 +0300

    day9 driver.java

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -1,12 +1,18 @@
 package com.cydeo.utilities;
 
+import org.openqa.selenium.WebDriver;
+
 public class Driver {
 
     /*
      Creating a private constructor, so we are closing access to the object of this class from outside of the class
      */
-    private Driver(){
+    private Driver(){}
 
-    }
+    /*
+    We make WebDriver private, because we want to close access from outside the class.
+    We make it static because we will use it in a static method
+     */
+    private static WebDriver driver;
 
 }
