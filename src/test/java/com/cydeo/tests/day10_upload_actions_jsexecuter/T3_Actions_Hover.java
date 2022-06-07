8b9e2c89641c8cac3384ede265f6b0642997188c
orhunpusat <orhunpusat3000@gmail.com>
2022-06-07 15:30:37 +0300

    day10 T3 actions_hover practice

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecuter/T3_Actions_Hover.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecuter/T3_Actions_Hover.java
@@ -33,17 +33,23 @@ public class T3_Actions_Hover {
 
         //3. Assert:
         //a. “name: user1” is displayed
-        //b. “view profile” is displayed
         Assert.assertTrue(user1.isDisplayed());
 
         //4. Hover over to second image
+        BrowserUtils.sleep(2);
+        actions.moveToElement(img2).perform();
+
         //5. Assert:
         //a. “name: user2” is displayed
-        //b. “view profile” is displayed
+        Assert.assertTrue(user2.isDisplayed());
+
         //6. Hover over to third image
+        BrowserUtils.sleep(2);
+        actions.moveToElement(img3).perform();
+
         //7. Confirm:
         //a. “name: user3” is displayed
-        //b. “view profile” is displayed
+        Assert.assertTrue(user3.isDisplayed());
 
 
 
